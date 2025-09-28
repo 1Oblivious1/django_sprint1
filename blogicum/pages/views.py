6236c2881111f3b4c8e5b84a15242df42ce34708
1Oblivious1 <146579310+1oblivious1@users.noreply.github.com>
2025-09-28 23:09:49 +0300

    Initial commit

---
+++ blogicum/pages/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def rules(request):
+    return render(request, 'pages/rules.html')
